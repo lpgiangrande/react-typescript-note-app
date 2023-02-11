f6fdc53ddb815ca82501fcc24040228f32be3ed1
Laura G <62339671+lpgiangrande@users.noreply.github.com>
2023-02-11 06:16:32 +0100

    add body and buttons

--- src/NoteForm.tsx
+++ src/NoteForm.tsx
@@ -1,4 +1,4 @@
-import { Form, Stack, Row, Col} from "react-bootstrap"
+import { Form, Stack, Row, Col, Button } from "react-bootstrap"
 import CreatableReactSelect from "react-select/creatable"
 
 export function NoteForm() {
@@ -19,6 +19,14 @@ export function NoteForm() {
                         </Form.Group>
                     </Col>
                 </Row>
+                <Form.Group controlId="markdown">
+                    <Form.Label>Body</Form.Label>
+                    <Form.Control required as="textarea" rows={15} />
+                </Form.Group>
+                <Stack direction="horizontal" gap={2} className="justify-content-center">
+                    <Button type="submit" variant="primary">Save</Button>
+                    <Button type="button" variant="outline-secondary">Cancel</Button>
+                </Stack>
             </Stack>
         </Form>
     )
